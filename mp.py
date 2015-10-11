0b2e86b1eedc3c682a97d66380271cc3dccfd3be
Petro Chechko <netzmb@gmail.com>
2015-10-11 03:21:34 +0300

    added a skeleton for multicore follower data processing

--- mp.py
+++ mp.py
@@ -1,50 +1,37 @@
-import multiprocessing as mp
-from follower import Follower
-from instadb import Session
+import multiprocessing as mps
 from lib import api
 
 
-session = Session()
+def parse_follower(follower_info):
+    # do some hard work here
+    return follower_info.username
 
 
-def get_followers(user_id):
-    followed_by, next_ = api.user_followed_by(user_id=user_id)
+def followers_iter(user_id):
+    more_followed_by, next_ = api.user_followed_by(user_id=user_id)
+
     while next_:
+        for follower in more_followed_by:
+            yield follower
+
         more_followed_by, next_ = api.user_followed_by(user_id=user_id, with_next_url=next_)
-        followed_by.extend(more_followed_by)
-    for follower in followed_by:
-        session.add(Follower(user_id=follower.id))
-        session.commit()
+
+    return
 
 
-# followed_by = get_followers('1921850126')
+def get_followers_info(user_id):
 
+    workers_pool = mps.Pool()
 
-def worker_func(x):
-    return x * x
-
-
-# def iter_followers():
-#     next_ = True
-#     while next_:
-#         more_followed_by, next_ = api.user_followed_by(user_id=user_id, with_next_url=next_)
-#     for follower in more_followed_by:
-#         session.add(Follower(user_id=follower.id))
-#         session.commit()
-#
-#     while ne
-#     followed_by, next_ = api.user_followed_by(user_id=user_id)
-
-
-
-def get_followers_by_pool():
-    workers_pool = mp.Pool(processes=5)
-
-    # task_data = xrange(1,500000)
-
-    for result in workers_pool.imap_unordered(worker_func, task_data):
-        followed_by
-        yield result
+    for follower_info in workers_pool.imap_unordered(parse_follower, followers_iter(user_id)):
+        yield follower_info
 
     workers_pool.close()
     return
+
+
+if __name__ == "__main__":
+
+    for info in get_followers_info(user_id=1921850126):
+        # do something with data which parsed by parse_follower
+        print info
