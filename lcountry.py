0823cd980cc4037ca3b0dbfce2fa6e58444fca73
Dmitriy Nyashkin <sealnetsoul@gmail.com>
2015-11-09 17:30:09 +0300

    add locate_country_code function

--- lcountry.py
+++ lcountry.py
@@ -1,6 +1,6 @@
 from follower import Follower
 from instadb import Session
-from getdatadb import locate_country
+from getdatadb import locate_country, locate_country_code
 
 
 session = Session()
@@ -11,6 +11,7 @@ def fill_countries():
     for follower in session.query(Follower).all():
         if follower.country is None:
             follower.country = locate_country(follower.latitude, follower.longitude)
+            follower.code = locate_country_code(follower.latitude, follower.longitude)
             session.commit()
             fill_countries()
 
