cde717af9241c67f383006d2b3c0acdeb0c619e2
Kirill G <kirill@speedflow.com>
2016-03-23 07:26:31 +0200

    Added locations parsing

---
+++ locs.py
@@ -0,0 +1,21 @@
+from instagram.client import InstagramAPI
+
+api = InstagramAPI(client_id='ade077a508f241b599aa55d924730a10', client_secret='85a2c94c85d844b79d39e86e7d8d84a7')
+
+from itertools import product
+import numpy as np
+
+
+class location(object):
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+coordinates = list(product(np.arange(50.40,50.47,0.005), np.arange(30.47,30.60,0.005)))
+
+locations_list = api.location_search(lat=coordinates[0][0],lng=coordinates[0][1])
+
+for coord in coordinates:
+    locations_list = api.location_search(lat=coord[0],lng=coord[1])
+    for location in locations_list:
+        print location.id
